cc52c8e25ec735742ee2221fee5e205f9d7ebb51
sbulla <sbulla@macbook-pro-di-c-rds-lnotaro.local>
2018-11-08 17:00:16 +0100

    Initial Commit

---
+++ WeeklySchedule/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeeklySchedule
+//
+//  Created by Silvio Bulla on 08/11/2018.
+//  Copyright © 2018 Silvio Bulla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeeklySchedule/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeeklySchedule
+//
+//  Created by Silvio Bulla on 08/11/2018.
+//  Copyright © 2018 Silvio Bulla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
